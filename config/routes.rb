b4bb12eec5fb4c9a80186000ba921e618784394c
shingo.kawamura <cyokotokoucya@outlook.jp>
2020-03-12 22:14:33 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-    root to: "toppages#index"
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+   
+  
+  
     root to: "tasks#index"
-    
-    get "login", to: "sessions#new"
-    post "login", to: "sessions#create" 
-    delete 'logout', to: 'sessions#destroy'
-
-    get 'signup', to: 'tasks#new'
-
-    resources :tasks, only: [:index, :show, :new, :create]
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create" 
+  delete 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new' 
+  
+  resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
