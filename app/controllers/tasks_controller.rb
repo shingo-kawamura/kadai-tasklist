f6ed9457022959ca750f2e40adadd7ded83833f5
shingo.kawamura <cyokotokoucya@outlook.jp>
2020-03-21 08:26:21 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,11 +8,11 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "Task が正常に投稿されました"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-   
-  
-  
-  root to: "tasks#index"
   root to: "toppages#index"
+  
   get "login", to: "sessions#new"
   post "login", to: "sessions#create" 
   delete 'logout', to: 'sessions#destroy'
