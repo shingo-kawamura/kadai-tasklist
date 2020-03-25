559586ce2f971aaa7c41fcade0fb17abcd97b980
shingo.kawamura <cyokotokoucya@outlook.jp>
2020-03-25 01:34:53 +0000

    修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,13 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
+  
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
+    #@tasks = Task.all
   end
 
   def show
@@ -19,7 +25,7 @@ class TasksController < ApplicationController
       redirect_to @task
     else
       flash.now[:danger] = "Task が投稿されませんでした"
-      render :new
+      render "tasks/new"
     end
   end
 
@@ -38,20 +44,38 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    #puts "ここを見ろ"
+    #p @task
+    
     @task.destroy
-
+    
     flash[:success] = "Task は正常に削除されました"
     redirect_to tasks_url
   end
   private
   
   def set_task
+    puts "set_task"
+    
     @task = Task.find(params[:id])
+    p @task
+    #@task.destroy
+      
+    #flash[:success] = "Task は正常に削除されました"
+    #redirect_to tasks_url
   end
   
   def task_params
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+    #@task = Task.find(params[:id])
+    #redirect_to root_url 
+  end
 
 end
